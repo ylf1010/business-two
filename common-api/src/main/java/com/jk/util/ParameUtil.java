d324aa94db9bf7bfc4bd5ab14534c208a69b86e4
ztx630824378 <630824378@qq.com>
2019-08-26 22:46:12 +0800

    张天祥0826最后一次提交

--- common-api/src/main/java/com/jk/util/ParameUtil.java
+++ common-api/src/main/java/com/jk/util/ParameUtil.java
@@ -40,6 +40,15 @@ public class ParameUtil implements Serializable {
     private Integer flid;
 
     private Integer ordernumber;
+    private Integer brandid;
+
+    public Integer getBrandid() {
+        return brandid;
+    }
+
+    public void setBrandid(Integer brandid) {
+        this.brandid = brandid;
+    }
 
     public Integer getOrdernumber() {
         return ordernumber;
